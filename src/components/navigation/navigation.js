c43ca287f7571773efda10a3bf3f318ca82a7b50
kieranmv95 <kieranmv95@gmail.com>
2018-06-26 19:39:22 +0100

    shuffled shit about

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,28 +1,35 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import styles from './navigation.module.css'
+import styles from './navigation.module.scss'
 
 class Navigation extends React.Component {
 
   constructor() {
     super();
     this.state = { navigation: false }
+    this.toggleNavigation = this.toggleNavigation.bind(this);
+  }
+
+  toggleNavigation() {
+    this.setState({ navigation: !this.state.navigation });
   }
 
   render() {
     return (
       <div className={!this.state.navigation ? styles.navigation : [styles.navigation, styles['navigation--active']].join(' ')}>
-        <div onClick={() => this.setState({ navigation: !this.state.navigation })} className={styles.navigation__icon}>
+        <div onClick={this.toggleNavigation} className={styles.navigation__icon}>
           <span></span>
           <span></span>
           <span></span>
         </div>
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/services/">Services</Link></li>
-            <li><Link to="/about/">About</Link></li>
-            <li><Link to="/about/">Contact</Link></li>
-        </ul>
+        <nav>
+          <ul>
+              <li><Link onClick={this.toggleNavigation} to="/">Home</Link></li>
+              <li><Link onClick={this.toggleNavigation} to="/services/">Services</Link></li>
+              <li><Link onClick={this.toggleNavigation} to="/about/">About</Link></li>
+              <li><Link onClick={this.toggleNavigation} to="/about/">Contact</Link></li>
+          </ul>
+        </nav>
       </div>
     )
   }
