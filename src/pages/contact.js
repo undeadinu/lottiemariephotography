c716b026371aa956d34bc607927bfef7989f90f9
kieranmv95 <kieranmv95@gmail.com>
2018-07-22 17:36:58 +0100

    added required fields

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,19 +9,19 @@ const ContactPage = () => (
             <p className="text--center">Fill out the form below and I will get back to you as quickly as possible.</p>
             <form className={styles.formHolder} method="POST" action="https://www.enformed.io/krf46brt">
                 <div className={styles.formGroup}>
-                    <label>Name</label>
-                    <input name="name" className={styles.formControl} type="text" placeholder="Name"/>
+                    <label>Name <sup className={styles.required}>*</sup></label>
+                    <input required name="name" className={styles.formControl} type="text" placeholder="Name"/>
                 </div>
                 <div className={styles.formGroup}>
-                    <label>Email Address</label>
-                    <input name="email" className={styles.formControl} type="text" placeholder="Email address"/>
+                    <label>Email Address <sup className={styles.required}>*</sup></label>
+                    <input name="email" required className={styles.formControl} type="text" placeholder="Email address"/>
                 </div>
                 <div className={styles.formGroup}>
-                    <label>Phone Number:</label>
+                    <label>Phone Number</label>
                     <input name="contact_number" className={styles.formControl} type="text" placeholder="Phone number"/>
                 </div>
                 <div className={styles.formGroup}>
-                    <p>Contact Preference:</p>
+                    <p>Contact Preference <sup className={styles.required}>*</sup></p>
                     <div className={styles.radioGroup}>
                         <label className={styles.customCheckBox}>Email
                             <input name="contact_preference" value="email" type="radio"/>
@@ -38,8 +38,8 @@ const ContactPage = () => (
                     </div>
                 </div>
                 <div className={`${styles.formGroup} ${styles.wideGroup}`}>
-                    <label>Message</label>
-                    <textarea name="message" className={styles.formControlTextArea} rows="4" cols="50"></textarea>
+                    <label>Message <sup className={styles.required}>*</sup></label>
+                    <textarea required name="message" className={styles.formControlTextArea} rows="4" cols="50"></textarea>
                 </div>
                 <div className={styles.formGroup}>
                     <button type="submit"className={styles.submitButton}>Submit</button>
